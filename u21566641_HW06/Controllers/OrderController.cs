a032c57a8756fc59fa4ef378fbcd7d273a5a11b4
U21566641 <u21566641@tuks.co.za>
2022-10-29 19:58:32 +0200

    orders page no pagination

---
+++ u21566641_HW06/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using u21566641_HW06.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace u21566641_HW06.Controllers
+{
+    public class OrderController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+        // GET: Order
+        public ActionResult Index()
+        {
+            var orders = db.orders.Include(o => o.order_items);
+
+            return View(orders.ToList());
+        }
+    }
+}
